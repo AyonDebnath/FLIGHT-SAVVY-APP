b9fbccab1818b96d6b161d8b7f8297ee5e7777c4
AyonDebnath <ayonnath.31@gmail.com>
2023-11-20 19:45:28 -0330

    UI building in progress...

--- flight_savvy/lib/view/profile_view.dart
+++ flight_savvy/lib/view/profile_view.dart
@@ -102,7 +102,7 @@ class ProfilePage extends StatelessWidget {
                 SizedBox(width: 50),
                 // Add some space between the button and the image
                 Image.asset(
-                  'assets/flightIcon.png',
+                  'assets/frontDesk.png',
                   // height: 21, // Set the desired height
                   width: 150, // Set the desired width
                 ),
@@ -110,7 +110,9 @@ class ProfilePage extends StatelessWidget {
             ),
           ),
           Container(
-            padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20, bottom: 20), // Adjust the left padding as needed
+            padding:
+                EdgeInsets.only(left: 20.0, right: 20.0, top: 20, bottom: 20),
+            // Adjust the left padding as needed
             child: const Text(
               "Book faster and manage your details more easily with an account.",
               style: TextStyle(
@@ -120,38 +122,205 @@ class ProfilePage extends StatelessWidget {
           ),
 
           Container(
-            padding: EdgeInsets.only(left: 20.0, bottom: 20), // Adjust the left padding as needed
+            padding: EdgeInsets.only(left: 20.0, bottom: 20),
+            // Adjust the left padding as needed
             child: const Text(
               "Manage your account",
               style: TextStyle(fontSize: 27.0, fontWeight: FontWeight.bold),
             ),
           ),
 
-
-          // Add cards for "Your details", "Your trips", etc.
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.account_circle),
-              title: Text('Your details'),
-              onTap: () {
-                // Handle "Your details" press
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: GridView.builder(
+              shrinkWrap: true,
+              // Use shrinkWrap to allow the ListView to take only the space it needs
+              physics: NeverScrollableScrollPhysics(),
+              // Disable scrolling for the GridView
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, // Number of columns in the grid
+                crossAxisSpacing: 15, // Spacing between columns
+                mainAxisSpacing: 15.0, // Spacing between rows
+              ),
+              itemBuilder: (context, index) {
+                // Your grid item widget
+                if (index == 0) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Handle tap on the grid item
+                      print('Tapped on item $index');
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black38,
+                        // Replace with your desired background color
+                        borderRadius: BorderRadius.circular(
+                            15), // Set the border radius for rounded corners
+                      ), // Replace with your desired background color
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/details.png',
+                            // height: 21, // Set the desired height
+                            width: 105, // Set the desired width
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            'Details',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 1) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Handle tap on the grid item
+                      print('Tapped on item $index');
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black38,
+                        // Replace with your desired background color
+                        borderRadius: BorderRadius.circular(
+                            15.0), // Set the border radius for rounded corners
+                      ), // Replace with your desired background color
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/trips.png',
+                            // height: 21, // Set the desired height
+                            width: 90, // Set the desired width
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text(
+                            'Your Trips',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 2) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Handle tap on the grid item
+                      print('Tapped on item $index');
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black38,
+                        // Replace with your desired background color
+                        borderRadius: BorderRadius.circular(
+                            15.0), // Set the border radius for rounded corners
+                      ), // Replace with your desired background color
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/settings.png',
+                            // height: 21, // Set the desired height
+                            width: 70, // Set the desired width
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Settings',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+                if (index == 3) {
+                  return GestureDetector(
+                    onTap: () {
+                      // Handle tap on the grid item
+                      print('Tapped on item $index');
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black38,
+                        // Replace with your desired background color
+                        borderRadius: BorderRadius.circular(
+                            15.0), // Set the border radius for rounded corners
+                      ), // Replace with your desired background color
+                      alignment: Alignment.center,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/support.png',
+                            // height: 21, // Set the desired height
+                            width: 150, // Set the desired width
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Text(
+                            'Support',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                }
               },
+              itemCount: 4, // Number of items in the grid
             ),
           ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.airplane_ticket),
-              title: Text('Your trips'),
-              onTap: () {
-                // Handle "Your trips" press
-              },
-            ),
-          ),
-          // ... Add more cards for other options
+          //
+          // // Add cards for "Your details", "Your trips", etc.
+          // Card(
+          //   child: ListTile(
+          //     leading: Icon(Icons.account_circle),
+          //     title: Text('Your details'),
+          //     onTap: () {
+          //       // Handle "Your details" press
+          //     },
+          //   ),
+          // ),
+          // Card(
+          //   child: ListTile(
+          //     leading: Icon(Icons.airplane_ticket),
+          //     title: Text('Your trips'),
+          //     onTap: () {
+          //       // Handle "Your trips" press
+          //     },
+          //   ),
+          // ),
+          // // ... Add more cards for other options
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: 'Home',
