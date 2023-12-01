2dd48a46a59979151bb308ab92fd6142ac1273e2
KenilShah <kvshah@mun.ca>
2023-12-01 19:34:58 -0330

    The files which were not pushed in the previous commits includes the flight card expandable and number of passenger selection.

---
+++ flight_savvy/lib/view/flightCardExpandable.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+
+class Expandable extends StatelessWidget {
+  final String airline;
+  final String flightNumber;
+  final String departure;
+  final String destination;
+  final String departureTime;
+  final String arrivalTime;
+  final String price;
+  final String duration;
+  final List<dynamic> segments;
+
+
+  Expandable({
+    required this.airline,
+    required this.duration,
+    required this.flightNumber,
+    required this.departure,
+    required this.destination,
+    required this.departureTime,
+    required this.arrivalTime,
+    required this.price,
+    required this.segments
+  });
+
+  String _getSegmentFlights() {
+    List<String> details = [];
+    for (var segment in this.segments) {
+      for (var data in segment) {
+        details.add(data.toString());
+        // print(data[data.length - 1]);
+      }
+    }
+    print(details[0]);
+    return details.join(''); // Join the list elements into a single string
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    print(segments);
+    return Container(
+      padding: const EdgeInsets.all(16),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.black12, Colors.black12],
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.blue.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Flight Details',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+              Icon(
+                Icons.local_atm,
+                color: Colors.white,
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          FlightDetailItem(label: 'Flights', value: _getSegmentFlights()),
+
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'CAD $price',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FlightDetailItem extends StatelessWidget {
+  final String label;
+  final String value;
+
+  FlightDetailItem({required this.label, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white70),
+        ),
+        Column(
+          children: [
+            Text(
+              value, // Join the list elements into a single string
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ],
+        ),
+        const SizedBox(height: 8),
+      ],
+    );
+  }
+}
+
+
+
