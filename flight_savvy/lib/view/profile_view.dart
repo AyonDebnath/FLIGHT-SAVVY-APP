8d1b6f5c06e3e947f36b0ddb9b8dc5a0e139009d
AyonDebnath <ayonnath.31@gmail.com>
2023-12-01 23:17:11 -0330

    Logout button moved

--- flight_savvy/lib/view/profile_view.dart
+++ flight_savvy/lib/view/profile_view.dart
@@ -1,10 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'homepage_view.dart';
-import 'login_view.dart';
+
 import 'details_view.dart';
-import 'support_view.dart';
 import 'settings_view.dart';
+import 'support_view.dart';
 
 class Profile extends StatefulWidget {
   final String username;
@@ -17,6 +15,7 @@ class Profile extends StatefulWidget {
 
 class _ProfileState extends State<Profile> {
   final String username;
+
   _ProfileState({required this.username});
 
   Widget build(BuildContext context) {
@@ -28,19 +27,8 @@ class _ProfileState extends State<Profile> {
 
 class ProfilePage extends StatelessWidget {
   final String username;
-  ProfilePage({required this.username});
 
-  void _signOut(BuildContext context) {
-    try {
-      FirebaseAuth.instance.signOut();
-      Navigator.of(context).pushAndRemoveUntil(
-        MaterialPageRoute(builder: (context) => LoginView()),
-        (Route<dynamic> route) => false,
-      );
-    } catch (e) {
-      // Handle errors
-    }
-  }
+  ProfilePage({required this.username});
 
   @override
   Widget build(BuildContext context) {
@@ -58,54 +46,14 @@ class ProfilePage extends StatelessWidget {
       ),
       body: ListView(
         children: [
-          // Add user greeting, login, and signup button
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: ButtonTheme(
-                    minWidth: 100.0, // Set the desired width
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        _signOut(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.black,
-                        // Set the background color of the button
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(
-                              10.0), // Set rounded corners
-                        ),
-                        elevation: 4.0, // Set the elevation of the button
-                      ),
-                      child: Container(
-                        padding: EdgeInsets.symmetric(
-                          vertical: 10.0,
-                        ),
-                        child: Text(
-                          'Log out',
-                          style: TextStyle(
-                            fontSize: 18.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white, // Set the text color
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 50),
-                // Add some space between the button and the image
-                Image.asset(
-                  'assets/frontDesk.png',
-                  // height: 21, // Set the desired height
-                  width: 150, // Set the desired width
-                ),
-              ],
-            ),
+          // Add user gr
+          SizedBox(width: 50),
+          // Add some space between the button and the image
+          Image.asset(
+            'assets/frontDesk.png',
+            height: 200,
           ),
+
           Container(
             padding:
                 EdgeInsets.only(left: 20.0, right: 20.0, top: 20, bottom: 20),
@@ -386,7 +334,6 @@ class ProfilePage extends StatelessWidget {
           // ... Add more cards for other options
         ],
       ),
-
     );
   }
 }
