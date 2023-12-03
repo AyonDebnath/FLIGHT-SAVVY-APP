fa6672bac9c7769c13da196d3f2280006f559957
AyonDebnath <ayonnath.31@gmail.com>
2023-12-03 20:28:34 -0330

    Sustainability view completed

--- flight_savvy/lib/view/sustainability.dart
+++ flight_savvy/lib/view/sustainability.dart
@@ -4,22 +4,66 @@ class sustainability extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Sustaibability'),
-          centerTitle: true,
-          bottom: PreferredSize(
-            preferredSize: Size.fromHeight(1.0),
-            child: Container(
-              color: Colors.black,
-              height: 1.0,
-            ), // Set the preferred size of the underline
+      appBar: AppBar(
+        title: Text('Sustaibability'),
+        backgroundColor: Colors.greenAccent,
+        centerTitle: true,
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(1.0),
+          child: Container(
+            color: Colors.black,
+            height: 1.0,
+          ), // Set the preferred size of the underline
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/sustainableBackground.jpeg'),
+            fit: BoxFit
+                .cover,
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Text(
-              'Our Steps Toward Sustainability.\n\nHelping every traveler explore our world effortlessly, for generations to come.\n\nWe are committed to helping shape a more responsible future for travel in collaboration with our partners.',
-              style: TextStyle(fontSize: 18)),
-        ));
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              Text('We’re committed to doing all that we can as a global business to reduce our own carbon footprint. ',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'We’re helping to ensure a positive future for destinations and local communities in every part of the world for generations to come. We know we’ve got a responsibility to our planet and each other, and together, as a group, we can be a catalyst for change.',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'We are committed to helping shape a more responsible future for travel in collaboration with our partners.',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
